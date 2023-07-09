35857b0ddb2ac1b8c93abaa47e68b71e560d90c4
alawllm <alicja.willam@gmail.com>
2023-07-09 17:13:19 +0200

    trying to fetch local data

---
+++ app.js
@@ -0,0 +1,30 @@
+
+import jsonData from "./data.json" assert { type: 'json' };
+
+console.log('hello world')
+
+
+// function makeBars(jsonData) {
+//     //find the max height for the bar
+//     //specify the ratio - amount / max amount
+//     //iterate over json array, set the height of the bars
+//     //find current date, highlight it with a different color
+//     let maxAmount = 0;
+
+//     for (let i = 0; i < fetchedData.length; i++) {
+//         const amount = fetchedData[i].amount
+//         if (amount > maxAmount) {
+//             maxAmount = amount;
+//         }
+//     }
+//     let height = 0;
+
+
+//     console.log(maxAmount)
+
+
+
+
+// }
+
+
